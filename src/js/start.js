3757c0471bbe9267ebe23a87074ed113305e6332
Soren <danilo-258@hotmail.com>
2025-02-16 03:21:06 -0300

    feat: add game initialization function in start.js

---
+++ src/js/start.js
@@ -0,0 +1,11 @@
+function iniciarJogo() {
+  let nivel = document.getElementById('nivel').value
+
+  if(nivel === '') {
+    alert('Selecione um nível para iniciar o jogo')
+    return false
+  }
+
+  window.location.href = 'game.html?' + nivel
+
+}
